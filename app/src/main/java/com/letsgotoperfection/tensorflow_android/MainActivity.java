91e9db35e1d00b1e3d6a933dd635a9d9f351b0ec
DevHossamHassan <hossamhassancs15@gmail.com>
2017-10-21 02:37:29 +0200

    Initial commit

---
+++ app/src/main/java/com/letsgotoperfection/tensorflow_android/MainActivity.java
@@ -0,0 +1,22 @@
+package com.letsgotoperfection.tensorflow_android;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+
+}
